dd0da1bf1ec36badaad3c170c2dccdcbaf3e4c29
hhcgit <hhc50331994@gmail.com>
2019-04-01 08:45:25 -0700

    namespace part 1 2

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,9 +1,20 @@
 'use strict';
+/*global cuid*/
 
 const Item = (function () {
 
-
-  return {
-    
+  function validateName(name){
+    if (name ===""){
+      throw new TypeError("Name does not exist");
+    }
+  }
+  function create(name){
+    return {
+      id:cuid(),
+      name:name,
+      checked:false
+    }
+  }
+  return {validateName,create
   };
 }() );
--- scripts/store.js
+++ scripts/store.js
@@ -4,10 +4,10 @@
 
 const store = ( function (){
   const items= [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
+    // { id: cuid(), name: 'apples', checked: false },
+    // { id: cuid(), name: 'oranges', checked: false },
+    // { id: cuid(), name: 'milk', checked: true },
+    // { id: cuid(), name: 'bread', checked: false }
   ];
   let hideCheckedItems= false;
   let searchTerm= '';
